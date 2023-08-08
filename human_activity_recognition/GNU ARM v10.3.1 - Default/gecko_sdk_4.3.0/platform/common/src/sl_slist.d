731ef0ec51f3e8ae8409838bd281f31db30c96ed
huydinhtien <tienhuynt12@gmail.com>
2023-08-08 16:47:21 +0700

    Remove duplicated folder

--- human_activity_recognition/GNU ARM v10.3.1 - Default/gecko_sdk_4.3.0/platform/common/src/sl_slist.d
+++
@@ -1,8 +0,0 @@
-gecko_sdk_4.3.0/platform/common/src/sl_slist.o: \
- C:/Users/anbinh/SimplicityStudio/SDKs/gecko_sdk/platform/common/src/sl_slist.c \
- c:\users\anbinh\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- c:\users\anbinh\simplicitystudio\sdks\gecko_sdk\platform\common\inc\sl_assert.h \
- c:\users\anbinh\simplicitystudio\sdks\gecko_sdk\platform\common\inc\sl_slist.h
-c:\users\anbinh\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-c:\users\anbinh\simplicitystudio\sdks\gecko_sdk\platform\common\inc\sl_assert.h:
-c:\users\anbinh\simplicitystudio\sdks\gecko_sdk\platform\common\inc\sl_slist.h:
--- human_activity_recognition/GNU ARM v10.3.1 - Default/gecko_sdk_4.3.0/platform/service/system/src/sl_system_process_action.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_4.3.0/platform/service/system/src/sl_system_process_action.o: \
- C:/Users/anbinh/SimplicityStudio/SDKs/gecko_sdk/platform/service/system/src/sl_system_process_action.c \
- c:\users\anbinh\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- D:\HUY\Documents\SSALab\2023\FPT\ IoT\ Challenge\ 2023\FPT-IoT-Challenge-2023_MSC\human_activity_recognition\autogen/sl_event_handler.h
-c:\users\anbinh\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-D:\HUY\Documents\SSALab\2023\FPT\ IoT\ Challenge\ 2023\FPT-IoT-Challenge-2023_MSC\human_activity_recognition\autogen/sl_event_handler.h:
